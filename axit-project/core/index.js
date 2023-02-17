0ff4b0ee0d883badec16678708bf7d761af1f10d
Natallia Andronovich <nandronovich@gmail.com>
2023-02-17 18:46:48 +0300

    added update burger__btn

--- axit-project/core/index.js
+++ axit-project/core/index.js
@@ -1,5 +1,7 @@
 const sidebar = document.querySelector(".sidebar");
+const burger = document.querySelector(".header__burger");
 
 const onBurgerClick = () => {
     sidebar.classList.toggle("sidebar_active");
+    burger.classList.toggle("header__burger_active");
 }
